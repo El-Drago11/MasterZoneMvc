4466b0bfc74645ca667babf5dd7767e40053cebc
11Rahulsinghrawat@gmail.com <11Rahulsinghrawat@gmail.com>
2024-08-01 15:40:38 +0530

    removed the comment

--- MasterZoneMvc/Scripts/js/business-panel/backup.js
+++ MasterZoneMvc/Scripts/js/business-panel/backup.js
@@ -329,11 +329,6 @@ function swl01() {
 
 }
 
-<<<<<<< HEAD
-//master : Unable to use loader 
-=======
-//master: Unable to use loader 
->>>>>>> master
 function getBusinessBackup() {
     //$('#dnwBackup').prop('disabled', true);
     if (global_AdminUserId !== undefined && global_AdminUserId !== null && !isNaN(global_AdminUserId)) {
